0649064b2e39ab3c90e5682f732687801cdbb42d
GorelovaP <komsomolskai1@mail.ru>
2022-09-18 23:54:25 +0300

    Refactoring add scss

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import s from "./Contact.module.css"
-import sComm from "../../common/style/Wrapper.module.css"
+import s from "./Contact.module.scss"
 import {Button} from "../../common/button/Button";
 import {Title} from "../../common/title/Title";
 
@@ -8,14 +7,20 @@ export const Contact = () => {
     return (
         <div className={s.contact}>
             <Title bgTitle={"contact"} mainTitle={"Get in "} spanText={"touch"}/>
-            <div className={`${sComm.wrapper} ${s.wrapper}`}>
-                <h2 className={s.h2}>Contacts</h2>
-                <form action="#" className={s.form}>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <textarea name="fild" className={s.textarea}> </textarea>
-                </form>
-                <Button type={"submit"} text={"Отправить"}/>
+            <div className={s.bodyWrapper}>
+                <div className={s.content}>
+                    <div className={s.blockInfo}>
+                        <h3 className={s.titleH3}>Don't be shy !</h3>
+                    </div>
+                    <div className={s.blockForm}>
+                        <form action="#" className={s.form}>
+                            <input type="text"/>
+                            <input type="text"/>
+                            <textarea name="fild" className={s.textarea}> </textarea>
+                        </form>
+                        <Button type={"submit"} text={"Отправить"}/>
+                    </div>
+                </div>
             </div>
         </div>
     );
